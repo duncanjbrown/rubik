87dd460c575ce205fb55bf451ce489f229a30dc7
Duncan Brown <duncanjbrown@gmail.com>
2017-04-23 23:44:22 +0100

    (feature) Implement rotate-x and rotate-xy
    These functions should have better names.
    
    rotate-x will rotate a set of cubies 90 degrees anticlockwise in the X-Y
    plane, and set the displayed colours.
    
    rotate-xy will do the rotation itself, whilst swap-faces will see to the
    colours.

--- src/rubik/core.clj
+++ src/rubik/core.clj
@@ -12,6 +12,19 @@
       (assoc-in [:colors face1] (get-in cubie [:colors face2]))
       (assoc-in [:colors face2] (get-in cubie [:colors face1]))))
 
+(defn rotate-xy
+  [cubie]
+  (update cubie :position
+          (fn [[x y z]]
+            (conj (rotate-90 [x y]) z))))
+
+(defn rotate-x
+  [cubies]
+  (map (fn [c]
+         (-> c
+             (rotate-xy)
+             ((partial swap-faces :x :y)))) cubies))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/rubik/core_test.clj
+++ test/rubik/core_test.clj
@@ -13,5 +13,39 @@
     (is (=
           {:colors
            {:x :red :y :blue}}
-          (swap-faces {:colors
-                       {:x :blue :y :red}} :x :y)))))
+          (swap-faces :x :y {:colors
+                       {:x :blue :y :red}}))))
+  
+  (testing "rotate-xy"
+    (is (=
+          {:position
+           [-1 1 1]}
+          (rotate-xy {:position [1 1 1]}))))
+
+  (testing "rotate-x"
+    (is (=
+         [
+          {:colors
+           {:x :red :y :green :z :white}
+           :position [1 -1 1]}
+          {:colors
+           {:x nil :y :green :z :white}
+           :position [1 0 1]}
+          {:colors
+           {:x :orange :y :green :z :white}
+           :position [1 1 1]}
+          ]
+         (rotate-x
+          [
+           {:colors
+            {:x :green :y :red :z :white}
+            :position [-1 -1 1]}
+           {:colors
+            {:x :green :y nil :z :white}
+            :position [0 -1 1]}
+           {:colors
+            {:x :green :y :orange :z :white}
+            :position [1 -1 1]}
+           ]
+           ))))
+  )
