c1f9cccd3e5ef1c83ffdc3c400b2bec5d476a5ff
Duncan Brown <duncanjbrown@gmail.com>
2017-04-23 23:42:37 +0100

    (fix) rotate-90 returns a vector
    We need to conj the z-axis value back onto the end of it after we've
    transformed xyz co-ordinates

--- src/rubik/core.clj
+++ src/rubik/core.clj
@@ -2,9 +2,9 @@
   (:require [clojure.core.matrix :as m])
   (:gen-class))
 
-(defn rotate-90 
+(defn rotate-90
   [vctr]
-  (map int (m/mmul [[0 -1] [1 0]] vctr)))
+  (vec (map int (m/mmul [[0 -1] [1 0]] vctr))))
 
 (defn swap-faces
   [cubie face1 face2]
